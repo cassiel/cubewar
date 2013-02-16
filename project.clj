27069ef2e5701aaeb7bc1f4e4be59da44c54d142
nick rothwell <nick@cassiel.com>
2013-02-16 17:05:57 +0000

    Basic ring/compojure setup.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject eu.cassiel/cubewar "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/cassiel/cubewar"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.5"]
+                 [net.loadbang/net.loadbang.osc "1.4.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.hsqldb/hsqldb "2.2.9"]]
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler cassiel.cubewar.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
---
+++ test/cassiel/cubewar/test/handler.clj
@@ -0,0 +1,14 @@
+(ns cassiel.cubewar.test.handler
+  (:use clojure.test
+        ring.mock.request
+        cassiel.cubewar.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
