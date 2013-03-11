c5048ddce0f80b9354309a7d3ca3343079375b6e
nick rothwell <nick@cassiel.com>
2013-03-11 17:10:25 +0000

    Bump to Clojure 1.5.1.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject eu.cassiel/cubewar "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "https://github.com/cassiel/cubewar"
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [slingshot "0.10.3"]
                  [net.loadbang/net.loadbang.osc "1.5.0"]
